fd9cd3d8bb664bb79cffa5b778e856eb97b68136
Thomas Mailund <tm@kvantify.dk>
2025-12-29 07:45:14 +0100

    Using the known number of intents and slightly better handling of reconciliation in the execute traversal

--- crates/actor/src/actor.rs
+++ crates/actor/src/actor.rs
@@ -9,15 +9,19 @@ impl<T> ActorAlgo for T where T: ExecuteTrait<ActorExecutionContext, Reconcile> 
 /// Reconciliation book-keeping (mock for now) and the consumer trait
 /// for invoking reconciliation on intents produced by the actor's block.
 pub struct Reconcile {
-    orders: Vec<Order>,
+    pub(super) orders: Vec<Order>,
     idx: usize,
 }
 impl Reconcile {
-    pub fn new() -> Self {
+    pub fn new(no_intents: usize) -> Self {
         Self {
-            orders: vec![],
+            orders: vec![Order::NoOrder; no_intents],
             idx: 0,
         }
+    }
+    pub fn reconcile(&mut self) -> &mut Reconcile {
+        self.idx = 0;
+        self
     }
     /// Process an intent into an order, given the previous order state.
     /// This is a mock implementation for now; the real implementation should decide
@@ -34,20 +38,12 @@ impl Reconcile {
         }
     }
 }
-impl Default for Reconcile {
-    fn default() -> Self {
-        Self::new()
-    }
-}
 
 /// Implement the IntentConsumerTrait for Reconcile to process intents.
 /// This gives us a callback that is invoked after each block's execution
 /// where we can reconsile and push order updates out.
 impl IntentConsumerTrait for Reconcile {
     fn consume(&mut self, intent: &Intent) {
-        if self.idx >= self.orders.len() {
-            self.orders.push(Order::NoOrder);
-        }
         self.orders[self.idx] = self.process_intent(&self.orders[self.idx], intent);
         self.idx += 1;
     }
@@ -67,10 +63,12 @@ where
     Algo: ActorAlgo,
 {
     pub fn new(id: u32, algo: Box<Algo>) -> Self {
+        let no_intents = algo.no_intents();
+        let reconcile = Reconcile::new(no_intents);
         Self {
             id,
             algo,
-            reconcile: Default::default(),
+            reconcile,
         }
     }
 
@@ -83,7 +81,7 @@ where
     }
 
     fn execute(&mut self, context: &ActorExecutionContext) -> Option<()> {
-        self.algo.execute(context, &mut self.reconcile)
+        self.algo.execute(context, self.reconcile.reconcile())
     }
 }
 
